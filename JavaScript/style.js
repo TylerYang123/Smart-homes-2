267f244ed66629b850382679a85a1bd773c3317f
TylerYang123 <Tyler.yang24@student.cart.org>
2025-05-30 10:38:50 -0700

    hi

--- JavaScript/style.js
+++ JavaScript/style.js
@@ -8,4 +8,24 @@ function handleSearch() {
       alert("Please enter a search term.");
     }
   }
-  
+
+  let currentSlide = 0;
+  const slides = document.querySelectorAll('.slide');
+
+  function showSlide(index) {
+    slides.forEach((slide, i) => {
+      slide.classList.remove('active');
+      if (i === index) {
+        slide.classList.add('active');
+      }
+    });
+  }
+
+  function changeSlide(direction) {
+    currentSlide = (currentSlide + direction + slides.length) % slides.length;
+    showSlide(currentSlide);
+  }
+
+  // Initial display
+  showSlide(currentSlide);
+
